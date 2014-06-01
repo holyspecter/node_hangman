0b9051fff143afcf9bca088a6d3fb87b17f56742
rok <lenivec17@gmail.com>
2014-06-01 23:04:54 +0300

    game init started

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ var app = require('express')(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
     words = [
-        'Kiev'
-    ];
+        'Kiev',
+        'Moscow',
+        'Paris'
+    ],
+    currentWord;
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    currentWord = words[0];
+    console.log('New game started. the word is: ' + currentWord);
+
+    io.emit('game.init_info', {
+        'wordLength': currentWord.length
+    });
 
     socket.on('game.input.char', function(char) {
         io.emit(-1 !== words[0].indexOf(char) ? 'game.input.right_char' : 'game.input.wrong_char');
@@ -30,3 +38,7 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+function random (low, high) {
+    return Math.random() * (high - low) + low;
+}
